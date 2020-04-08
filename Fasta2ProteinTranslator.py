bedc7ba93d8378ba4ef321f73ac40e236e7da711
sunyumail93 <sunyumail93@gmail.com>
2020-04-08 10:35:38 -0400

    Add files via upload

---
+++ Fasta2ProteinTranslator.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+#Fasta2ProteinTranslator.py
+#This script takes DNA fasta seq as input, finds the first ATG and translates it until the stop codon
+#Version: Y.H.S, 2020-04-08
+                                                                          
+import sys
+
+def translate(sequence):
+    codontable = {
+    'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+    'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+    'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+    'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
+    'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+    'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+    'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+    'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+    'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+    'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+    'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+    'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+    'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+    'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+    'TAC':'Y', 'TAT':'Y', 'TAA':'*', 'TAG':'*',
+    'TGC':'C', 'TGT':'C', 'TGA':'*', 'TGG':'W',
+    }
+
+    proteinsequence = ''
+    sequence=sequence.upper()
+    start = sequence.find('ATG')
+    if start >= 0:
+        SequenceRange = sequence[int(start):]
+        endTAA = sequence.find('TAA')
+        endTAG = sequence.find('TAG')
+        endTGA = sequence.find('TGA')
+        end=max(endTAA,endTAG,endTGA)
+        for n in range(0,len(SequenceRange),3):
+            if SequenceRange[n:n+3] in codontable:
+                proteinsequence += codontable[SequenceRange[n:n+3]]
+            if proteinsequence[-1] == '*':
+                break
+    else:
+        proteinsequence += '-'
+    return(proteinsequence)
+
+def Fasta2ProteinTranslator():
+    fi=open(sys.argv[1],'r')
+    lines=fi.readlines()  #read all lines into array
+    for line in lines:
+        if line[0] == '>':
+            print(line.strip())
+        else:
+            seq=translate(line.strip())
+            print(seq.strip())
+
+if len(sys.argv) != 2: #if the length of argv is not equal to 2, then print warning message
+    print "This script takes DNA fasta seq as input, finds the first ATG and translates it until the stop codon"
+    print "This script can only output the first found open reading frame (start from the first ATG)"
+    print "If no open reading frame found, it will return -; If no stop codon found, it will translate to the last possible codon"
+    print "Usage: [TranslateFASTA2Protein.py] [Fasta]"
+else:
+    Fasta2ProteinTranslator()
